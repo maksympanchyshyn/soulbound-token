73e955d00dacb49735756b0b1e255dc8e951623e
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2023-01-18 15:41:28 +0200

    Add  event. Remove mintBatch function

--- contracts/Badge.sol
+++ contracts/Badge.sol
@@ -8,6 +8,8 @@ import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol';
 contract Badge is ERC1155, AccessControl, ERC1155Supply {
   bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
 
+  event BadgeClaimed(uint256 indexed badgeId, address indexed to, uint256 amount);
+
   constructor() ERC1155('') {
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _grantRole(MINTER_ROLE, msg.sender);
@@ -15,21 +17,13 @@ contract Badge is ERC1155, AccessControl, ERC1155Supply {
 
   function mint(
     address account,
-    uint256 id,
+    uint256 badgeId,
     uint256 amount,
     bytes memory data
   ) public onlyRole(MINTER_ROLE) {
-    require(balanceOf(account, id) == 0, 'Badge already minted');
-    _mint(account, id, amount, data);
-  }
-
-  function mintBatch(
-    address to,
-    uint256[] memory ids,
-    uint256[] memory amounts,
-    bytes memory data
-  ) public onlyRole(MINTER_ROLE) {
-    _mintBatch(to, ids, amounts, data);
+    require(balanceOf(account, badgeId) == 0, 'Badge already minted');
+    _mint(account, badgeId, amount, data);
+    emit BadgeClaimed(badgeId, account, amount);
   }
 
   // The following functions are overrides required by Solidity.
@@ -43,7 +37,7 @@ contract Badge is ERC1155, AccessControl, ERC1155Supply {
   ) internal override(ERC1155, ERC1155Supply) {
     require(
       (from == address(0) && to != address(0)) || (from != address(0) && to == address(0)),
-      'Transfer not allowed'
+      'Transfer is not allowed'
     );
     super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
   }
