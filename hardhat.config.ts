0f22c4574c852af9dfad4d52c5af6af084f1df80
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2023-01-18 19:24:06 +0200

    Add hardhat and environment configution

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from 'dotenv';
+
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: '0.8.17',
+  networks: {
+    goerli: {
+      url: process.env.GOERLI_URL || '',
+      accounts: [process.env.PRIVATE_KEY || ''],
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: 'USD',
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
