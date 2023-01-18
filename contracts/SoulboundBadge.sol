889049280e5cbfca33c1e78fadd30e5789f47c2d
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2023-01-18 15:11:04 +0200

    Move from ERC721 to ERC1155 as better option for badge case

--- contracts/SoulboundBadge.sol
+++ contracts/SoulboundBadge.sol
@@ -1,37 +1,54 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
+import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol';
 
-contract Badge is ERC721, AccessControl {
+contract Badge is ERC1155, AccessControl, ERC1155Supply {
   bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
 
-  constructor() ERC721('Badge', 'BDG') {
+  constructor() ERC1155('') {
     _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     _grantRole(MINTER_ROLE, msg.sender);
   }
 
-  function safeMint(address to, uint256 tokenId) public onlyRole(MINTER_ROLE) {
-    _safeMint(to, tokenId);
+  function mint(
+    address account,
+    uint256 id,
+    uint256 amount,
+    bytes memory data
+  ) public onlyRole(MINTER_ROLE) {
+    require(balanceOf(account, id) == 0, 'Badge already minted');
+    _mint(account, id, amount, data);
   }
 
+  function mintBatch(
+    address to,
+    uint256[] memory ids,
+    uint256[] memory amounts,
+    bytes memory data
+  ) public onlyRole(MINTER_ROLE) {
+    _mintBatch(to, ids, amounts, data);
+  }
+
+  // The following functions are overrides required by Solidity.
   function _beforeTokenTransfer(
+    address operator,
     address from,
     address to,
-    uint256 tokenId,
-    uint256 batchSize
-  ) internal override(ERC721) {
+    uint256[] memory ids,
+    uint256[] memory amounts,
+    bytes memory data
+  ) internal override(ERC1155, ERC1155Supply) {
     require(
       (from == address(0) && to != address(0)) || (from != address(0) && to == address(0)),
       'Transfer not allowed'
     );
-    super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
   }
 
-  // The following functions are overrides required by Solidity.
-
-  function supportsInterface(bytes4 interfaceId) public view override(ERC721, AccessControl) returns (bool) {
+  function supportsInterface(bytes4 interfaceId) public view override(ERC1155, AccessControl) returns (bool) {
     return super.supportsInterface(interfaceId);
   }
 }
