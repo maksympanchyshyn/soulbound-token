d1586bced0caa6a324a0cae59c959cf4bc698b12
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2023-01-18 15:52:39 +0200

    Add signer and badgeAddress getters/setters to the BadgeClaimer.sol

--- contracts/BadgeMinter.sol
+++ contracts/BadgeMinter.sol
@@ -1,6 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-contract BadgeMinter {
-  constructor() {}
+import '@openzeppelin/contracts/access/AccessControl.sol';
+
+contract BadgeMinter is AccessControl {
+  address public badgeAddress;
+  address public signer;
+
+  constructor() {
+    _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+  }
+
+  function setBadgeAddress(address _badgeAddress) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+    badgeAddress = _badgeAddress;
+  }
+
+  function setSigner(address _signer) external virtual onlyRole(DEFAULT_ADMIN_ROLE) {
+    signer = _signer;
+  }
+
+  function getBadgeAddress() public view virtual returns (address) {
+    return badgeAddress;
+  }
+
+  function getSigner() public view virtual returns (address) {
+    return signer;
+  }
 }
