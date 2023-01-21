f5844aa66dd6460c558b96cd13b51ce995fe5597
Maksym Panchyshyn <maksymm.panchyshyn@gmail.com>
2023-01-21 18:30:59 +0200

    Add badges publishing functionality to the Badge contract

--- contracts/Badge.sol
+++ contracts/Badge.sol
@@ -1,32 +1,86 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
+import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/access/AccessControl.sol';
+import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol';
 
-contract Badge is ERC1155, AccessControl, ERC1155Supply {
-  bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+import 'hardhat/console.sol';
 
+contract Badge is ERC1155, AccessControl, ERC1155Supply {
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  string private _uri;
+  mapping(uint256 => string) public ipfsHashMap;
+
+  event BadgePublished(address indexed publisher, uint256 indexed badgeId, string ipfsHash);
   event BadgeClaimed(uint256 indexed badgeId, address indexed to, uint256 amount);
 
-  constructor() ERC1155('') {
+  bytes32 public constant MINTER_ROLE = keccak256('MINTER_ROLE');
+  bytes32 public constant PUBLISHER_ROLE = keccak256('PUBLISHER_ROLE');
+
+  uint256 internal constant PUBLISHER_OFFSET_MULTIPLIER = uint256(2) ** (256 - 160);
+
+  constructor(string memory _initUri) ERC1155('') {
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    _uri = _initUri;
   }
 
-  function mint(
-    address account,
-    uint256 badgeId,
-    uint256 amount,
-    bytes memory data
-  ) public onlyRole(MINTER_ROLE) {
+  function setMinterRole(address _account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    _grantRole(MINTER_ROLE, _account);
+  }
+
+  function setPublisher(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    _grantRole(PUBLISHER_ROLE, account);
+  }
+
+  function setURI(string memory _newUri) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    _uri = _newUri;
+  }
+
+  function publish(string memory ipfsHash) public onlyRole(PUBLISHER_ROLE) {
+    address sender = msg.sender;
+    _publish(sender, ipfsHash);
+  }
+
+  function mint(address account, uint256 badgeId, uint256 amount, bytes memory data) public onlyRole(MINTER_ROLE) {
     require(balanceOf(account, badgeId) == 0, 'Badge already minted');
     _mint(account, badgeId, amount, data);
     emit BadgeClaimed(badgeId, account, amount);
   }
 
-  function setMinterRole(address _account) external onlyRole(DEFAULT_ADMIN_ROLE) {
-    _grantRole(MINTER_ROLE, _account);
+  function uri(uint256 badgeId) public view override returns (string memory) {
+    require(wasPublished(badgeId), 'uri: this badge id was never published');
+    return _getFullUri(badgeId);
+  }
+
+  function wasPublished(uint256 badgeId) public view returns (bool) {
+    console.log(getIpfsHashByBadgeId(badgeId));
+    return keccak256(abi.encodePacked(getIpfsHashByBadgeId(badgeId))) != keccak256(abi.encodePacked(''));
+  }
+
+  function getIpfsHashByBadgeId(uint256 badgeId) public view returns (string memory) {
+    return ipfsHashMap[badgeId];
+  }
+
+  function _publish(address publisher, string memory ipfsHash) private {
+    _tokenIds.increment();
+    uint256 newTokenId = _tokenIds.current();
+    // owner (160) / Counter Token ID (96)
+    uint256 _badgeId = uint256(uint160(publisher)) *
+      PUBLISHER_OFFSET_MULTIPLIER + // Publisher
+      newTokenId; // Counter Token ID
+    console.log('Badge ID: %s', _badgeId);
+    ipfsHashMap[_badgeId] = ipfsHash;
+
+    emit BadgePublished(publisher, _badgeId, ipfsHash);
+  }
+
+  function _getFullUri(uint256 badgeId) private view returns (string memory) {
+    string memory _ipfsHash = getIpfsHashByBadgeId(badgeId);
+    return string(abi.encodePacked(_uri, _ipfsHash));
   }
 
   // The following functions are overrides required by Solidity.
